d7fc2cdac751ca51f3f209f163dba22fb79cf815
Nikita Soia <nikitasoia@MacBook-Pro-Nikita.local>
2019-10-15 18:28:04 +0300

    connect webpack with background

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,11 +2,11 @@ import React, { lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './app.scss';
+import 'antd/dist/antd.css';
 import '../assets/styles/_reset.scss';
 import '../assets/styles/_fonts.scss';
 
 const Header = lazy(() => import('../layouts/header'));
-const Footer = lazy(() => import('../layouts/footer'));
 const HomePage = lazy(() => import('../../components/pages/home-page'));
 const SomePage = lazy(() => import('../../components/pages/some-page'));
 
@@ -18,7 +18,6 @@ const App = () => {
                 <Route path="/somepage" component={SomePage} />
                 <Route component={HomePage} />
             </Switch>
-            <Footer />
         </Router>
     );
 };
--- src/components/layouts/header/header.jsx
+++ src/components/layouts/header/header.jsx
@@ -1,17 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import SelectLangeage from '../../language';
+import { Select } from 'antd';
+
+import logo from '../../assets/images/logo.svg';
+import settings from '../../assets/images/settings.svg';
+import btc from '../../assets/images/coins/btc.svg';
+import dsh from '../../assets/images/coins/dsh.svg';
+import eth from '../../assets/images/coins/eth.svg';
+import ltc from '../../assets/images/coins/ltc.svg';
+import ecr from '../../assets/images/coins/ecr.svg';
 
 import style from './header.module.scss';
+import './header.scss';
 
 const Header = () => {
     const { t } = useTranslation();
+    const { Option } = Select;
+
+    const handleChange = value => {
+        console.log(value, 'value');
+    };
+
     return (
         <header className={style.header}>
-            <Link to="/">{t('test.header')}</Link>
-
-            <SelectLangeage />
+            <Link to="/">
+                <img src={logo} className={style.header__logo} alt="logo" />
+            </Link>
+            <Select
+                defaultValue="BTC"
+                className="header__dropDown"
+                onChange={handleChange}
+            >
+                <Option value="BTC">
+                    <img className={style.header__dropDown_img} src={btc} alt="btc" /> BTC, Bitcoin
+                </Option>
+                <Option value="DASH">
+                    <img className={style.header__dropDown_img} src={dsh} alt="dsh" /> DASH, Dash
+                </Option>
+                <Option value="ETH">
+                    <img className={style.header__dropDown_img} src={eth} alt="eth" /> ETH, Ethereum
+                </Option>
+                <Option value="LTC">
+                    <img className={style.header__dropDown_img} src={ltc} alt="ltc" /> LTC, Litecoin
+                </Option>
+                <Option value="ECRO">
+                    <img className={style.header__dropDown_img} src={ecr} alt="ecr" /> ECRO, Ecro
+                </Option>
+            </Select>
+            <Link to="/somepage">
+                <img src={settings} className={style.header__logo} alt="settings" />
+            </Link>
         </header>
     );
 };
